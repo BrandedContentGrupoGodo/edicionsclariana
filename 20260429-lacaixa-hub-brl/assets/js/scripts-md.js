5d332d05e39708453b03ad4f5bd591363dc29012
BrandedContentGrupoGodo <153719709+BrandedContentGrupoGodo@users.noreply.github.com>
2026-09-17 09:02:26 +0200

    Refactor star background and update historias

--- 20260429-lacaixa-hub-brl/assets/js/scripts-md.js
+++ 20260429-lacaixa-hub-brl/assets/js/scripts-md.js
@@ -101,7 +101,12 @@ const historias = [
     imagen: "https://brandedcontentgrupogodo.github.io/ext/caixabank-2025/01_09-07-25/assets/img/cap9.png",
     enlace: "https://www.mundodeportivo.com/juegos-olimpicos/20260625/1004198507/araceli-menduina-final-piraguismo-54-anos-constelaciones-streaming-brl.html"
   },
-  { locked: true },
+  {
+    nombre: "Martín de la Puente",
+    frase: "El primer bote de mi vida fue aceptar la discapacidad y normalizarla",
+    imagen: "https://brandedcontentgrupogodo.github.io/ext/caixabank-2025/01_09-07-25/assets/img/cap10.png",
+    enlace: "https://www.mundodeportivo.com/juegos-olimpicos/20260917/1004227901/martin-puente-tenista-espanol-aprovecho-segundo-bote-bola-constelaciones-streaming-brl.html"
+  },
   { locked: true },
   { locked: true }
 ];
